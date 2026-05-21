907c5c61da8c09b94f3a680b403b92fb14ac0205
Francis E Padua <paduaf@uci.edu>
2026-05-20 17:25:33 -0700

    chore: update index entrypoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * The entrypoint for the action. This file simply imports and runs the action's
  * main logic.
  */
-import { run } from './main.js'
+import { checkSecurityPolicyGuard } from './main.js'
 
 /* istanbul ignore next */
-run()
+checkSecurityPolicyGuard()
